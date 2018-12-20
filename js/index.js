60eeabc59173dcc972df35c20359de1bb718dda0
eslam-ashraf <43147649+eslam-ashraf@users.noreply.github.com>
2018-12-20 21:01:32 +0200

    Update index.js
    eslam section

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function MyMouseOut()
+{
+	n = n + 1 ;
+	document.getElementById("img1").innerHTML= n ;
+}
+function MMouseOut()
+{
+	n = n + 1 ;
+	document.getElementById("img2").innerHTML= n ;
+}
+function yMouseOut()
+{
+	n = n + 1 ;
+	document.getElementById("img3").innerHTML= n ;
+}
